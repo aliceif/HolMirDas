fade95bc88e104f33f76bf78f84a8866497f54e4
aliceif <7098860+aliceif@users.noreply.github.com>
2024-02-17 17:38:41 +0100

    add more plans

--- src/HolMirDas/Program.cs
+++ src/HolMirDas/Program.cs
@@ -1,5 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Starting HolMirDas");
+
+// configs needed:
+// list of urls (with source type)
+// target instance
+// access token
+// size of buffer (some servers have no storage)
+// location of buffer -> this should be a somewhat persistent temp file -> look up if this concept exists 
 
 // to load RSS
 // SyndicationFeed#Load takes XML Reader
@@ -7,8 +14,13 @@ Console.WriteLine("Hello, World!");
 // In there -> Items
 // Source -> of SyndicationItem member collection links first item? (type should I think be alternate, maybe query that)
 
+
+// bonus - can we pre-screen the incoming links against something like a local ringbuffer to prevent repeatedly ap/get-ing the same posts?
+
 // to post to misskey
 // simple post request to /ap/get with auth intact -> check how auth works, probably something with the app key token thing?^
 // seems like auth is just an added property in request json named "i" and takes the access token
 // requested remote post url is property "uri"
 // for testing probably do separate user.
+
+Console.WriteLine("HolMirDas finished");
